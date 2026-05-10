17a0e709d2bbbb1c6eefa5a4cbcd5e720ebc70e4
Conrad <conradkaranja5@gmail.com>
2026-05-10 23:56:34 +0300

    feat(auth): add protected dashboard routing with createBrowserRouter and connect LoginPage to Redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 
-function App() {
-  
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-  return (
-    <>
-     
-    </>
-  )
+import LoginPage from './Pages/LoginPage'
+import Dashboard from './Pages/Dashboard'
+import ProtectedRoute from './Components/Auth/ProtectedRoute'
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/login',
+      element: <LoginPage />,
+    },
+    {
+      path: '/',
+      element: <LoginPage />,
+    },
+    {
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: '/dashboard',
+          element: <Dashboard />,
+        },
+      ],
+    },
+  ])
+
+  return <RouterProvider router={router} />
 }
 
 export default App
+
